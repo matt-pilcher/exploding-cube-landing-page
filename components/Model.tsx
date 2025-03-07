c45557558ea90e967adbac9283b668643a1daa6d
matt-pilcher <mpilcher90@gmail.com>
2025-03-07 17:01:47 -0500

    add animation event to the cube on mousedown
    this animation was built in blender and exported to the glb file as an action. the js here only triggers the animation.

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,41 +1,59 @@
-import { useGLTF } from "@react-three/drei"
+import { useAnimations, useGLTF, CycleRaycast } from "@react-three/drei"
 import { useFrame, useThree } from "@react-three/fiber"
-import { useRef } from "react"
-import { Group } from "three"
-// TODO preload still works even when different file
+import { useMotionValue, useSpring, useTransform } from "framer-motion"
+import { useEffect, useRef } from "react"
+import { AnimationAction, Group, MeshPhysicalMaterial } from "three"
 useGLTF.preload("/cracked-cube.glb")
 
-
 export default function Model()  {
-  const group = useRef<Group>(null!);
-  const {viewport} = useThree();
-  const { nodes, materials, animations, scene } = useGLTF(
-    "/cracked-cube.glb"
-  )
+    const motionVal = useMotionValue(0)
+  
+    const group = useRef<Group>(null!);
+    const {viewport} = useThree();
+    const { nodes, materials, animations, scene } = useGLTF(
+      "/cracked-cube.glb"
+    )
+  
+    const spring = useSpring(motionVal, { stiffness: 50});
+    const { actions, clips } = useAnimations(animations, scene)
+    
+  
+    // Apply the material to each mesh in the GLTF model
+  
+    const physicalMaterial = new MeshPhysicalMaterial({
+      color: 0x1338BE,
+      roughness: 0.5,
+      clearcoat: 0.9,
+      metalness: 0.8,
+    });
+    // TODO: modify this so that the material is not overwriting the emission cube
+    useEffect(() => {
+      scene.traverse((child) => {
+        //@ts-ignore
+        if (child.isMesh && child.name !== "Cube") {
+          //@ts-ignore
+          child.material = physicalMaterial;
+        }
+      });
+    }, [scene]);
+  
+  
+    useFrame(({camera}) => {
+      group.current.rotation.y += 0.001;
+      Object.keys(actions).forEach((key) => {
+        const action = actions[key] as AnimationAction
+        action.play().paused = true
+        action.time = spring.get()
+      })
+    });
 
-  useFrame(({camera}) => {
-    group.current.rotation.y += 0.001;
-  });
-  const state = useThree();
-  console.log(scene);
-//  const gltfTest = useAnimations(animations, scene)
-// console.log(gltfTest);
-  return (
-    // face , intersections, objects.name all inside the raycaster class
-    // CycleRaycast pulls the object info in a similar way
-    // https://codesandbox.io/p/sandbox/ls503?file=%2Fsrc%2FApp.js%3A24%2C56 this uses event.stoppropagation
-
-    //https://react.dev/reference/react/useImperativeHandle
-    /**
-     * Do not overuse refs. 
-     * You should only use refs for imperative behaviors that you can't express as props: for example, scrolling to a node, 
-     * focusing a node, triggering an animation, selecting text, and so on.
-     * If you can express something as a prop, you should not use a ref. For example, 
-     * instead of exposing an imperative handle like { open, close } from a Modal component, it is better to take isOpen as a 
-     * prop like <Modal isOpen={isOpen} />. Effects can help you expose imperative behaviors via props.
-     */
-    <group scale={viewport.width / viewport.height} ref={group}>
-      <primitive object={scene} />
-    </group>
-  )
-}
+    return (
+      <group
+        onPointerMove={({ object }) => {
+          console.log(object.name)
+        }} 
+        onPointerUp={() => motionVal.set(0)} onPointerDown={() => motionVal.set(1)}  scale={viewport.width / viewport.height} ref={group}>
+        <primitive object={scene} />
+      </group>
+    )
+  }
--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useThree } from "@react-three/fiber"
 import Model from "./Model"
-import { Suspense } from "react"
-import { useProgress, Html, PerspectiveCamera, OrbitControls } from "@react-three/drei"
+import { Suspense, useRef } from "react"
+import { useProgress, Html, PerspectiveCamera, OrbitControls, CycleRaycast } from "@react-three/drei"
 
 function Loader() {
   const { progress, active } = useProgress()
@@ -13,9 +13,6 @@ function Loader() {
 
 export default function Scene() {
   return (
-    // h-svh
-    // TODO: Two extra divs are added to this canvas. The width is a hardcoded value overflowing the container
-    // Update the projection matrix on resize. Layout on mobile is correct on first load
     <Canvas gl={{ antialias: true }} dpr={[1, 1.5]} className="relative">
       <directionalLight position={[0, 0, 4]} intensity={8} color={0xffffff} />
       <directionalLight position={[-3,0, 4]} intensity={8} color={0x1338BE} />
